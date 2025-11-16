b7069194a274c0f861c1b025b891fde237e1dc79
kopachalmtd <joykamaa433@gmail.com>
2025-11-16 08:26:48 +0300

    Create movies.js

---
+++ data/movies.js
@@ -0,0 +1,8 @@
+// data/movies.js
+// Each movie costs KES 10
+const movies = [
+  { id: "m1", title: "Movie One", price: 10, poster: "/poster1.jpg", tg_file_id: "FILE_ID_1" },
+  { id: "m2", title: "Movie Two", price: 10, poster: "/poster2.jpg", tg_file_id: "FILE_ID_2" }
+];
+
+export default movies;
